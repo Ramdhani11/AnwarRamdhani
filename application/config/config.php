b3b365ec610022ccbd1f2c4c5c31ab57b2f2bea5
Ramdhani11 <anwarramdhanie@gmail.com>
2021-11-20 14:52:30 +0700

    Mengedit Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Semester 3/Pustaka_Booking/';
+$config['base_url'] = 'http://localhost/AnwarRamdhani/';
 
 /*
 |--------------------------------------------------------------------------
